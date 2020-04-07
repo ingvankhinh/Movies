7b9eec0b711c2f9b944de62dd478ac35b1dc3306
ingvankhinh <47348064+ingvankhinh@users.noreply.github.com>
2020-04-07 15:00:17 +0700

    Added all the codes

---
+++ index.js
@@ -0,0 +1,133 @@
+const autoCompleteconfig = {
+	renderOption: (movie) => {
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		return `
+			<img src="${imgSrc}">${movie.Title}(${movie.Year})
+		`;
+	},
+	inputValue(movie) {
+		return movie.Title;
+	},
+	async fetchData(searchTerm) {
+		const response = await axios.get('http://www.omdbapi.com/', {
+			params: {
+				apikey: '56408f1a',
+				s: searchTerm
+			}
+		});
+		if (response.data.Error) {
+			return [];
+		}
+		return response.data.Search;
+	}
+};
+
+createAutoComplete({
+	root: document.querySelector('#left-autocomplete'),
+	...autoCompleteconfig,
+	onOptionSelect(movie) {
+		document.querySelector('.tutorial').classList.add('is-hidden');
+		onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
+	}
+});
+createAutoComplete({
+	root: document.querySelector('#right-autocomplete'),
+	...autoCompleteconfig,
+	onOptionSelect(movie) {
+		document.querySelector('.tutorial').classList.add('is-hidden');
+		onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
+	}
+});
+
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '56408f1a',
+			i: movie.imdbID
+		}
+	});
+	summaryElement.innerHTML = movieTemplate(response.data);
+	if (side === 'left') {
+		leftMovie = response.data;
+	} else {
+		rightMovie = response.data;
+	}
+	if (leftMovie && rightMovie) {
+		runComparison();
+	}
+};
+
+function runComparison() {
+	const leftSideStats = document.querySelectorAll('#left-summary .notification');
+	const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+	leftSideStats.forEach((leftStat, index) => {
+		const rightStat = rightSideStats[index];
+
+		const leftSideValue = parseInt(leftStat.dataset.value);
+		const rightSideValue = parseInt(rightStat.dataset.value);
+		if (rightSideValue > leftSideValue) {
+			leftStat.classList.remove('is-primary');
+			leftStat.classList.add('is-warning');
+		} else {
+			rightStat.classList.remove('is-primary');
+			rightStat.classList.add('is-warning');
+		}
+	});
+}
+
+const movieTemplate = (movieDetail) => {
+	const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+	const metaScore = parseInt(movieDetail.Metascore);
+	const imdbRating = parseFloat(movieDetail.imdbRating);
+	const imdbVotes = parseFloat(movieDetail.imdbVotes.replace(/,/g, ''));
+
+	const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+		const value = parseInt(word);
+		if (isNaN(value)) {
+			return prev;
+		} else {
+			return prev + value;
+		}
+	}, 0);
+	console.log(awards);
+	return `
+		<article  class="media">
+			<figure class="media-left">
+				<p class="image">
+					<img src="${movieDetail.Poster}"></img>
+				</p>
+			</figure>
+			<div class="media-content">
+				<div class="content">
+					<h1>${movieDetail.Title}</h1>
+					<h4>${movieDetail.Genre}</h4>
+					<p>${movieDetail.Plot}</p>
+				</div>
+			</div>
+		</article>
+		<article data-value=${awards} class="is-primary notification">
+			<p class="title">${movieDetail.Awards}</p>
+			<p class="subtitle">Awards</p>
+		</article>
+		<article data-value=${dollars} class="is-primary notification">
+			<p class="title">${movieDetail.BoxOffice}</p>
+			<p class="subtitle">Box Office</p>
+		</article>
+		<article data-value=${metaScore} class="is-primary notification">
+			<p class="title">${movieDetail.Metascore}</p>
+			<p class="subtitle">Meta Scores</p>
+		</article>
+		<article data-value=${imdbRating} class="is-primary notification">
+			<p class="title">${movieDetail.imdbRating}</p>
+			<p class="subtitle">IMDB Rating</p>
+		</article>
+		<article data-value=${imdbVotes}  class="is-primary notification">
+			<p class="title">${movieDetail.imdbVotes}</p>
+			<p class="subtitle">IMDB Rating</p> 
+		</article>
+	`;
+};
---
+++ utils.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay) => {
+	let timeoutID;
+	return (...args) => {
+		if (timeoutID) {
+			clearTimeout(timeoutID);
+		}
+		timeoutID = setTimeout(() => {
+			func(...args);
+		}, delay);
+	};
+};
